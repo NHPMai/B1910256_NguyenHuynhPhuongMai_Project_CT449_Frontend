0dce3f7a572f08b7d6ba59885356b79fc5a08a9e
Nguyen Huynh Phuong Mai <nguyenmai16062001@gmail.com>
2023-05-05 01:02:24 +0700

    Hoan Thanh Du An

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="main-footer footer-dark" style="color:dimgrey;">
+        <footer>
+            <div class="container">
+                <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6 ">
+                    <div  style=" ; padding: 40px 0px 0px 0px ">
+                        <h6 style="font-size: 25px;font-weight: bold; font-style: normal; font-family: 'Arial'">WORKING HOURS</h6>
+                        <ul style="padding-left: 0px;">
+                            <li style="list-style: none;">Monday - Friday: 08:00 am - 08:30 pm</li>
+                            <li style="list-style: none;">Saturday: 10:00 am - 16:30 pm</li>
+                            <li style="list-style: none;">Sunday: 10:am - 16h30 pm </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div style="text-align: center" >
+                        <div >
+                            <a href="#">
+                                <img src="../assets/icons/logo2.png" alt="" height="140" width="100">
+                            </a>
+                        </div>
+                        <p >
+                            Tiệm bánh MaiNgân_Bakery mang đam mê và gửi gắm yêu thương thông qua những chiếc bánh ngọt ngào 
+                        </p>
+                        <div >
+                            <i class="top-header-icon-social fab fa-facebook-f "></i> &nbsp &nbsp
+                            <i class="top-header-icon-social fab fa-twitter"></i> &nbsp &nbsp
+                            <i class="top-header-icon-social fab fa-linkedin-in"></i> &nbsp &nbsp
+                            <i class="top-header-icon-social  fab fa-instagram"></i>                         
+                        </div>
+                    </div>    
+                </div>
+
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div style=" ; padding: 40px 0px 0px 50px">
+                        <h6 style="font-size: 25px;font-weight: bold; font-style: normal;font-family: 'Arial;">SUBSCRISE</h6>
+                        <p>Nhập email để nhận ưu đãi mới.</p>
+                        <form>
+                            <input type="text" placeholder="Email" style="height: 50px; width: 100%; padding-left: 22px; background: rgba(253, 243, 234, 0.2); border: none; color: #ffffff; font-size: 14px;">
+                        
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- <div ><a href="#logo"><img src="image1/top.jpg" alt="" height="105px" class="rounded" id="top" style="box-shadow: 3px 388888 !important;"></a> </div> -->
+        </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+
+</script>
